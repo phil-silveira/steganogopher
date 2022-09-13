0fcd706ffd54b07b86c2b32b8e14f91f859df2fb
Phil Silveira <phills.silveira@gmail.com>
2022-09-13 16:13:24 -0300

    📝  improve docs

--- cmd/steganogopher/main.go
+++ cmd/steganogopher/main.go
@@ -7,14 +7,21 @@ import (
 	"strings"
 )
 
+var commands = map[string]string{
+	"d": "decode data from <path/to/file.png>",
+	"e": "encode data into <path/to/file.png>",
+	"o": "path to the output .PNG file. Default value is <INPUT>_X.PNG",
+	"m": "message to be encoded",
+}
+
 func main() {
 	var encodeTarget, decodeTarget string
 	var outputPath, message string
 
-	flag.StringVar(&decodeTarget, "d", "", "decode data from <path/to/file.png>")
-	flag.StringVar(&encodeTarget, "e", "", "encode data into <path/to/file.png>")
-	flag.StringVar(&outputPath, "o", "", "path to the output .PNG file. Default value is <INPUT>_X.PNG")
-	flag.StringVar(&message, "m", "", "message to be encoded")
+	flag.StringVar(&decodeTarget, "d", "", commands["d"])
+	flag.StringVar(&encodeTarget, "e", "", commands["e"])
+	flag.StringVar(&outputPath, "o", "", commands["o"])
+	flag.StringVar(&message, "m", "", commands["m"])
 	flag.Parse()
 
 	decodeTarget = strings.Trim(decodeTarget, " ")
@@ -32,5 +39,10 @@ func main() {
 
 		fmt.Printf("{\"message\": \"%s\"}\n", msg)
 		return
+	}
+
+	fmt.Printf("Steganogopher is a steganography tool to play with .PNG images.\n\nUsage:\n\n\tsteganogopher <command> [arguments]\n\nThe commands are:\n\n")
+	for k, v := range commands {
+		fmt.Printf("\t-%s \t%s\n", k, v)
 	}
 }
